9aac21369a0fd9ea6abe445a818c92ba351a9749
mayuzumi1272 <117078422+mayuzumiDev@users.noreply.github.com>
2024-10-18 18:20:50 +0800

    Update urls.py
    fixed merge conflict

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import AccountLoginView, AccountLogoutView, PasswordResetRequestView, PasswordResetVerify, PasswordResetConfirm, OTPCodeResend
-
+ 
 urlpatterns = [
-    path('account-login/', AccountLoginView.as_view(), name="account_login"),
-    path('account-logout/', AccountLogoutView.as_view(), name='account_logout'),
-    path('password-reset-request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
-    path('password-reset-verify/', PasswordResetVerify.as_view(), name='password_reset_verify'),
-    path('password-reset-confirm/', PasswordResetConfirm.as_view(), name='password_reset_confirm'),
-    path('otp-resend/', OTPCodeResend.as_view(), name='otp_code_resend'),
+    path('account-login/', views.AccountLoginView.as_view(), name="account_login"),
+    path('account-logout/', views.AccountLogoutView.as_view(), name='account_logout'),
+    path('password-reset-request/', views.PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset-verify/', views.PasswordResetVerify.as_view(), name='password_reset_verify'),
+    path('password-reset-confirm/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    path('otp-resend/', views.OTPCodeResend.as_view(), name='otp_code_resend'),
 ]
