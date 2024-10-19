9e1f446ab03cd8960319665abdcdbca54999547d
mayuzumi1272 <117078422+mayuzumiDev@users.noreply.github.com>
2024-10-20 00:39:40 +0800

    Added TeacherListView
    Created TeacherListSerializer and TeacherListView

--- backend/user_admin/urls.py
+++ backend/user_admin/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.account.account_views import CreateAccountView
+from .views.account.account_views import CreateAccountView, TeacherListView
 
 urlpatterns = [
-    path("create-account/", CreateAccountView.as_view(), name='create_account')
+    path("create-account/", CreateAccountView.as_view(), name='create_account'),
+    path('teacher-list/', TeacherListView.as_view(), name='teacher_list')
 ]
