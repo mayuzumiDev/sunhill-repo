de5fdc16f227be199480c9b80c7ffa1d451f816d
mayuzumi1272 <117078422+mayuzumiDev@users.noreply.github.com>
2024-11-18 20:40:00 +0800

    Added backend logic for fetching users per branch

--- backend/user_admin/urls.py
+++ backend/user_admin/urls.py
@@ -3,6 +3,7 @@ from .views.current_admin_views import *
 from .views.account.account_views_create import *
 from .views.account.account_views_list import *
 from .views.account.account_views_edit import *
+from .views.branches.user_count_views import *
 from .views.account.pdf_accounts import *
 from .views.profile.admin_profile_views import AdminProfileImageView
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path("custom-user/delete/", CustomUserDeleteView.as_view(), name="custom_user_delete"),
     path("custom-user/edit/<int:pk>/", CustomUserEditView.as_view(), name='custom_user_edit'),
     path("user-info/edit/<int:pk>/", UserInfoEditView.as_view(), name='user_info_edit'),
+    path('user-info/profile-image/', AdminProfileImageView.as_view(), name='admin_profile_image'),
     path("student-info/edit/<int:pk>/", StudentInfoEditView.as_view(), name='student_info_edit'),
     path("parent-info/edit/<int:pk>/", ParentInfoEditView.as_view(), name='parent_info_edit'),
     path("teacher-info/edit/<int:pk>/", TeacherInfoEditView.as_view(), name='teacher_info_edit'),
@@ -22,5 +24,5 @@ urlpatterns = [
     path('student-list/', StudentListView.as_view(), name='student_list'),
     path('parent-list/', ParentListView.as_view(), name='parent_list'),
     path('public-user-list/', PublicUserListView.as_view(), name='public_user_list'),
-    path('user-info/profile-image/', AdminProfileImageView.as_view(), name='admin_profile_image'),
+    path('branch/user-list/', BranchUserCountView.as_view(), name='branch_user_list'),
 ]
