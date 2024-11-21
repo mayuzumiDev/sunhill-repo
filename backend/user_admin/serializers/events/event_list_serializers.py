cc7cdee5552028818b738c90f349c1ae258d6ab3
mayuzumi1272 <117078422+mayuzumiDev@users.noreply.github.com>
2024-11-21 16:24:47 +0800

    Fix event deletion error

---
+++ backend/user_admin/serializers/events/event_list_serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from user_admin.models.event_models import Event
+
+class EventListSerializer(serializers.ModelSerializer):
+    formatted_date = serializers.SerializerMethodField()
+    formatted_created_at = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Event
+        fields = ['id', 'title', 'description', 'date', 'target_audience', 'location', 'attachment', 'expiry_date', 'created_at', 'formatted_date', 'formatted_created_at']
+
+    def get_formatted_date(self, obj):
+        return obj.date.strftime('%B %d, %Y %I:%M %p')
+
+    def get_formatted_created_at(self, obj):
+        return obj.created_at.strftime('%d-%m-%Y %H:%M')
--- backend/user_admin/serializers/events/event_manage_serializers.py
+++ backend/user_admin/serializers/events/event_manage_serializers.py
@@ -36,16 +36,3 @@ class EventEditSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Event date cannot be in the past")
         return value
 
-class EventListSerializer(serializers.ModelSerializer):
-    formatted_date = serializers.SerializerMethodField()
-    formatted_created_at = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Event
-        fields = ['id', 'title', 'description', 'date', 'target_audience', 'location', 'attachment', 'expiry_date', 'created_at', 'formatted_date', 'formatted_created_at']
-
-    def get_formatted_date(self, obj):
-        return obj.date.strftime('%B %d, %Y %I:%M %p')
-
-    def get_formatted_created_at(self, obj):
-        return obj.created_at.strftime('%d-%m-%Y %H:%M')
